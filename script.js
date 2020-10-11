655640e3bb49eb523e0e5b02a91988b23f7ce06f
kokocat1313 <66399733+kokocat1313@users.noreply.github.com>
2020-10-11 00:02:44 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//function personalInfo(firstName, lastName, age) {
+   //return `Your first name is ${firstName}, Your last name ${lastName}, Your age is ${age}`
+//};
+
+//console.log(personalInfo('John', 'Smith', 45));
+
+let firstName = prompt("Your First Name");
+let lastName = prompt("Your Last Name");
+let age = prompt("Your Age");
+let fullName = firstName + lastName;
+
+console.log(`Your Full Name is ${fullName}`)
+console.log(`Your age is ${age}`)
